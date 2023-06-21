915ac014becfd63b549def788804dc0f1e08d238
Mohamed Salem <simulator_2004@hotmail.com>
2023-06-21 16:57:42 +0300

    first comm

---
+++ constant.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    name: "Omen",
+    artwork: "/Assets/omen",
+    abilities: [
+      {
+        abilityName: "shrouded step",
+        artwork: "/Assets/omen/ability1",
+        description:
+          "EQUIP a shrouded step ability and see its range indicator. FIRE to begin a brief channel, then teleport to the marked location.",
+        key: "",
+        credits: null,
+        cooldown: null,
+        duration: null,
+      },
+      {
+        abilityName: "paranoia",
+        artwork: "/Assets/omen/ability2",
+      },
+      {
+        abilityName: "dark cover",
+        artwork: "/Assets/omen/ability3",
+      },
+      {
+        abilityName: "from the shadows",
+        artwork: "/Assets/omen/ability4",
+      },
+    ],
+  },
+];
