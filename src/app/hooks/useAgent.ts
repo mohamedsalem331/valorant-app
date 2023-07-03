9c0eec1f449cc2e1ac476eba7f8e9f872b57704d
Mohamed Salem <simulator_2004@hotmail.com>
2023-07-04 01:22:05 +0300

    Update useAgent.ts

--- src/app/hooks/useAgent.ts
+++ src/app/hooks/useAgent.ts
@@ -1,13 +1,17 @@
 import { valorantAgents } from "../../../constant";
 
 const useAgent = async (agent: keyof typeof valorantAgents = "omen") => {
-  const res: any = await fetch(`http://127.0.0.1:3000/api/agent/${agent}`, {
-    next: { revalidate: 3 * 24 * 60 * 60 }, // Revalidate every 3 days
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const res: any = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/agent/${agent}`,
+    {
+      next: { revalidate: 3 * 24 * 60 * 60 }, // Revalidate every 3 days
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   return res.json();
 };
+
 export default useAgent;
